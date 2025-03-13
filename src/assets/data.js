09f5a642c862dd3ebc9c412254163cffc02b320b
Jitesh Kumar <jiteshsaini2005@gmail.com>
2025-03-13 23:22:34 +0530

    first commit55

--- src/assets/data.js
+++ src/assets/data.js
@@ -23,14 +23,7 @@ export const data = [
       option4: "Bin",
       ans: 1,
     },
-    {
-      question: "How much brain is missing in Disksha...?",
-      option1: "25%",
-      option2: "50%",
-      option3: "75%",
-      option4: "100%",
-      ans: 4,
-    },
+     
     {
       question: "A computer cannot BOOT if it does not have the?",
       option1: "Application Software",
